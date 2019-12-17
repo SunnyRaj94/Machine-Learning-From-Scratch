278b33b4b7bf771f25d442ecdf8e184adbfd1969
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-17 15:18:37 +0530

    find probability of drawing an ace from pack of cards

---
+++ probability_statistics/01_drawing_ace.py
@@ -0,0 +1,14 @@
+"""
+Created on 17/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a program to find probability of drawing an ace from pack of cards
+"""
+#we know pack of cards have 52 cards
+pack=52
+#number of ace in pack are 4
+number_of_aces=4
+#probability of finding an ace in pack is
+print("probability of finding an ace in pack is : ",number_of_aces/pack)
