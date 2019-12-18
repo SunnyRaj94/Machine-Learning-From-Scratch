a382b7f878581695f22582c76556c18f60913194
SunnyRaj <sunnyraj94@hotmail.com>
2019-12-18 15:07:25 +0530

    find the probability of getting a random number from the interval [2, 7]

---
+++ probability_statistics/12_random_number_probability.py
@@ -0,0 +1,12 @@
+"""
+Created on 18/12/2019
+@author: Sunny Raj
+"""
+"""
+problem statement:
+Write a program to find the probability of getting a random number from the interval [2, 7]
+"""
+#we know numbers between 2 to 7 are : 2,3,4,5,6,7 total 6 numbers
+total_numbers =6
+#so finding the probability to pick any one number is 1/6
+print("probability of picking any number between 2 and 7 is :",1,"/",total_numbers)
