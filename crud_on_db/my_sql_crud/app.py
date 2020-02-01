483ff8b84a32a3712569ea0392bf1effdeb0c741
sunny raj <sunnyraj94@hotmail.com>
2020-02-01 14:55:31 +0530

    refactored: made a more general api and removed all hardcoded indexes

--- crud_on_db/my_sql_crud/app.py
+++ crud_on_db/my_sql_crud/app.py
@@ -2,13 +2,15 @@
 #importing flask , jsonify,request
 from flask import Flask,  jsonify, request
 #importing mysql database library
-import MySQLdb
+# import MySQLdb
+import mysql.connector
 #seeting name to run this file as flask app
 app = Flask(__name__)
 
 #confugring my database here
-db = MySQLdb.connect("localhost", "root", "", "BookManager")
+# db = MySQLdb.connect("localhost", "root", "", "BookManager")
 
+db = mysql.connector.connect(host='localhost',user="root",passwd="PWD",database="Bookmanager")
 #method sends a get type request and obtains the list of tables present in current database
 @app.route('/tables', methods=['GET'])
 def get_tables():
@@ -38,6 +40,7 @@ def get_bookstore_table():
         curs.execute("select * from bookstore")
         # obtaining result after executing query
         myresult = curs.fetchall()
+        print(type(myresult))
         # converting the result into json
         return jsonify(myresult)
     except:
@@ -53,9 +56,9 @@ def add_new_book():
     # initializing database cursor
     curs = db.cursor()
     #fetching the data we have to add from request body
-    add_data = list(request.json)
+    add_data = request.json
     try:
-        query="insert into bookstore(book_name,author,gener,pub_year,price) values('{}','{}','{}','{}',{});".format(add_data[1],add_data[2],add_data[3],add_data[4],add_data[5])
+        query="insert into bookstore(book_name,author,gener,pub_year,price) values('{}','{}','{}','{}',{});".format(add_data['book_name'],add_data['author'],add_data['gener'],add_data['pub_year'],add_data['price'])
         # giving query to cursor to execute
         curs.execute(query)
         # giving query to cursor to execute
@@ -66,7 +69,7 @@ def add_new_book():
         return jsonify(myresult)
     except:
         print("Error: unable to fetch items")
-    return get_bookstore_table()
+    return "unable to add requested book into database"
 
 
 
@@ -77,9 +80,10 @@ def delete_book():
     # initializing database cursor
     curs = db.cursor()
     # fetching the data we have to delete from request body
-    delete_data = list(request.json)
+    delete_data = request.json
+    column_name =  delete_data.keys()
     try:
-        query="delete from bookstore where book_name = '{}';".format(delete_data[1])
+        query="delete from bookstore where {} = '{}';".format(column_name,delete_data[column_name])
         # giving query to cursor to execute
         curs.execute(query)
         # giving query to cursor to execute
@@ -90,45 +94,24 @@ def delete_book():
         return jsonify(myresult)
     except:
         print("Error: unable to fetch items")
-    return get_bookstore_table()
-
-
-#method sends a post type of request ,
-# executes the updation query and returns the whole table with updated values
-@app.route('/update_name', methods=['POST'])
-def update_book_name():
-    # initializing database cursor
-    curs = db.cursor()
-    # fetching the data we have to update from request body
-    update_data = list(request.json)
-    try:
-        #updating the name value
-        name_query="UPDATE bookstore SET book_name = {} WHERE book_name = {};".format(update_data[1],update_data[0])
-        # giving query to cursor to execute
-        curs.execute(name_query)
-        # giving query to cursor to execute
-        curs.execute("select * from bookstore")
-        # obtaining result after executing query
-        myresult = curs.fetchall()
-        # converting the result into json
-        return jsonify(myresult)
-    except:
-        print("Error: unable to fetch items")
-    return get_bookstore_table()
-
-
+    return "Unable to delete requested data from database"
 
 #method sends a post type of request ,
 # executes the updation query and returns the whole table with updated values
-@app.route('/update_author', methods=['POST'])
-def update_book_author():
+@app.route('/update', methods=['POST'])
+def update_bookstore():
     # initializing database cursor
     curs = db.cursor()
     # fetching the data we have to update from request body
-    update_data = list(request.json)
+    update_data = request.json
+    #extracting column names that are to be updated
+    # we will take column name , value before updation , value after updation in json format request
+    key = update_data['key']
+    value_before = update_data['value before']
+    updated_value = update_data['value to update']
     try:
-        #updating the author column value
-        author_query ="UPDATE bookstore SET author = {} WHERE author = {};".format(update_data[1],update_data[0])
+        # updating the author column value
+        author_query = "UPDATE bookstore SET {} = {} where {} = '{}';".format(key,updated_value,key,value_before)
         # giving query to cursor to execute
         curs.execute(author_query)
         # giving query to cursor to execute
@@ -139,80 +122,8 @@ def update_book_author():
         return jsonify(myresult)
     except:
         print("Error: unable to fetch items")
-    return get_bookstore_table()
+    return "Value Updation Failed"
 
-
-#method sends a post type of request ,
-# executes the updation query and returns the whole table with updated values
-@app.route('/update_gener', methods=['POST'])
-def update_book_gener():
-    # initializing database cursor
-    curs = db.cursor()
-    # fetching the data we have to update from request body
-    update_data = list(request.json)
-    try:
-        #updating the author column value
-        author_query ="UPDATE bookstore SET gener = {} WHERE gener = {};".format(update_data[1],update_data[0])
-        # giving query to cursor to execute
-        curs.execute(author_query)
-        # giving query to cursor to execute
-        curs.execute("select * from bookstore")
-        # obtaining result after executing query
-        myresult = curs.fetchall()
-        # converting the result into json
-        return jsonify(myresult)
-    except:
-        print("Error: unable to fetch items")
-    return get_bookstore_table()
-
-
-
-#method sends a post type of request ,
-# executes the updation query and returns the whole table with updated values
-@app.route('/update_pub', methods=['POST'])
-def update_book_pub_year():
-    # initializing database cursor
-    curs = db.cursor()
-    # fetching the data we have to update from request body
-    update_data = list(request.json)
-    try:
-        #updating the author column value
-        pub_query ="UPDATE bookstore SET pub_year = {} WHERE pub_year = {};".format(update_data[1],update_data[0])
-        # giving query to cursor to execute
-        curs.execute(pub_query)
-        # giving query to cursor to execute
-        curs.execute("select * from bookstore")
-        # obtaining result after executing query
-        myresult = curs.fetchall()
-        # converting the result into json
-        return jsonify(myresult)
-    except:
-        print("Error: unable to fetch items")
-    return get_bookstore_table()
-
-
-#method sends a post type of request ,
-# executes the updation query and returns the whole table with updated values
-@app.route('/update_price', methods=['POST'])
-def update_book_price():
-    # initializing database cursor
-    curs = db.cursor()
-    # fetching the data we have to update from request body
-    update_data = list(request.json)
-    try:
-        #updating the author column value
-        price_query ="UPDATE bookstore SET price = {} WHERE price = {};".format(update_data[1],update_data[0])
-        # giving query to cursor to execute
-        curs.execute(price_query)
-        # giving query to cursor to execute
-        curs.execute("select * from bookstore")
-        # obtaining result after executing query
-        myresult = curs.fetchall()
-        # converting the result into json
-        return jsonify(myresult)
-    except:
-        print("Error: unable to fetch items")
-    return get_bookstore_table()
 
 #base url directed towards getting whole table
 @app.route('/')
